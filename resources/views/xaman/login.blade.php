f6ac2c1cff995900228e26aa43c73228f597f900
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2025-08-04 23:17:18 +0200

    login via xaman app op mobile

--- resources/views/xaman/login.blade.php
+++ resources/views/xaman/login.blade.php
@@ -14,9 +14,11 @@
             </form>
         @else
             <div>
-                <h1>Login with xaman</h1>
-                <p>Scan the QR code below to log in:</p>
-                <img src="{{ $qr }}" alt="xaman Login QR Code">
+                <h1 class="text-2xl font-bold mb-2">Login with Xaman</h1>
+                <img src="{{ $qr }}" class="w-full" alt="xaman Login QR Code">
+                <a href="xumm://xumm.app/detect" class="mt-4 inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 sm:hidden">
+                    Open Xaman
+                </a>
             </div>
 
             <script>
