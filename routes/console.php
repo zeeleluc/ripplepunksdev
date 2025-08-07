3f711bdbcd1e5ecf1b85ebf504fe418c524f9048
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2025-08-07 23:56:13 +0200

    no message

--- routes/console.php
+++ routes/console.php
@@ -9,5 +9,5 @@ if (app()->environment('prod')) {
     $schedule->command('nfts:sync', [
         '--issuer' => 'r3SvAe5197xnXvPHKnyptu3EjX5BG8f2mS',
         '--taxon' => '604',
-    ])->hourly();
+    ])->everyTwentyMinutes();
 }
