d98a3bb3a4e9190550e14c71a779458c8e5ae371
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2025-08-08 00:35:24 +0200

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,6 +61,13 @@
     </div>
 </header>
 
+    @if (request()->is('/'))
+        <div class="bg-red-500 shadow text-sm text-white text-center px-6 py-2 border-b font-bold">
+            This isn’t the CTO version—this is the original RipplePunks by The Dev, now expanding to 20,000.
+            How and why? Browse through the <a href="{{ route('logs') }}">logs</a> to find out.
+        </div>
+    @endif
+
     @auth
         <div class="bg-white shadow text-sm text-gray-700 px-6 py-2 flex items-center justify-between border-b">
             <span class="truncate max-w-[60%] overflow-hidden whitespace-nowrap">
