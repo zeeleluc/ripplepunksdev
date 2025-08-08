43c55a274eaaf99f3874108525de56b7415d7b88
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2025-08-08 14:52:07 +0200

    no message

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -4,8 +4,8 @@
 
         <nav class="space-x-4 mt-3 md:mt-0">
             <a href="/" class="hover:text-white transition">Home</a>
-            <a href="/about-cto" class="hover:text-white transition">About the CTO</a>
-            <a href="/giveaway" class="hover:text-white transition">Giveaway</a>
+{{--            <a href="/about-cto" class="hover:text-white transition">About the CTO</a>--}}
+            <a href="/claim" class="hover:text-white transition">Claim Rewards</a>
             <a href="/holders" class="hover:text-white transition">Holders</a>
         </nav>
     </div>
