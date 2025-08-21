d47d097fdf318b0b25eb84c800728ceee2d049dd
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2025-08-21 16:35:51 -0400

    no message

--- app/Models/Shout.php
+++ app/Models/Shout.php
@@ -6,13 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shout extends Model
 {
-    protected $fillable = [
-        'wallet',
-        'message',
-    ];
+    protected $fillable = ['wallet', 'message'];
 
     public function user()
     {
         return $this->belongsTo(User::class, 'wallet', 'wallet');
+    }
+
+    public function holder()
+    {
+        return $this->belongsTo(Holder::class, 'wallet', 'wallet');
     }
 }
