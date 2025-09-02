a6f18a4003feb922d4144ec6fb258f92f509bfca
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2025-09-01 20:35:06 -0400

    no message

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,9 +8,6 @@ class TestController extends Controller
 {
     public function index()
     {
-        $user = User::find(auth()->id());
-        dd($user->xumm_token);
-
         return view('test');
     }
 }
